3b7b2ebb016f73ac5da6e6c80235b37edf8be251
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-06-21 00:10:55 +0200

    added axios-react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+// bootstrap'in script tesekkür linkini index.html'e, fontAwesome'in tesekkür linkini de index.html'e eklendi.
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
